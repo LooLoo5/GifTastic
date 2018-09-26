6d82042e143b34f07db2b2fb2b038649bc6ef600
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-09-26 16:56:36 -0500

    it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,20 @@
 var topics = ["Dog", "Cat", "Horse"];
 
 $("button").on("click", function() {
-})
+  var animal = $(this).attr("data-animal");
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&api_key6c4JhykYadmEOL8spw1wMdsMcNrqPX1r";
+        animal + "&api_key=6c4JhykYadmEOL8spw1wMdsMcNrqPX1r";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
-    });
 
-    for (var i = 0; i < results.length; i++) {
-        
+      for (var i = 0; i < response.data.length; i++) {
+      var gif = $("<img>").attr("src",response.data[i].images.original.url);
+      $("#gifs-appear-here").append(gif);
     }
+    });
+  })
